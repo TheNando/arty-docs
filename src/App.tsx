de6100a46c03e5360a17ebdae99fac476a78f266
Nando Pena <fernando.pena@rackspace.com>
2019-04-06 22:07:06 -0500

    update doc structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Nav from "./Nav";
 import Page from "./Page";
-import logo from "./logo.svg";
+
+const INDEX_URL =
+  "https://github.rackspace.com/api/v3/repos/CARE/arty-docs/contents/docs/index.md?access_token=0ca7dc5a20542093a1b6daf90f918e74e88069ff";
 
 class App extends Component {
+  async componentDidMount() {
+    const response = await fetch(INDEX_URL);
+    const meta = await response.json();
+    console.log(atob(meta.content));
+  }
+
   render() {
     return (
       <HashRouter>
